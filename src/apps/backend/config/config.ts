f5b362fe25a46a3cc6abc086c674490ec57ba970
Mozzarth <moisescaicedo15@gmail.com>
2021-02-04 12:47:39 -0500

    FIX file config

--- src/apps/backend/config/config.ts
+++ src/apps/backend/config/config.ts
@@ -2,11 +2,12 @@ import { config } from 'dotenv'
 
 if (process.env.NODE_ENV != "production") {
     const con = config()
-    if (con.error)} {
-    console.error("Ocurrio un error a la hora de configurar entorno")
-    process.exit(1)
+    if (con.error) {
+        console.error("Ocurrio un error a la hora de configurar entorno")
+        process.exit(1)
+    }
 }
-}
+
 
 export const CONFIG = {
     SERVER_PORT: process.env.NODE_PORT
