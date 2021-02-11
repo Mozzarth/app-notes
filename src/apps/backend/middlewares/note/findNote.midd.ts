efe14233798fecdc3c0c81be442deea8494eead9
Mozzarth <moisescaicedo15@gmail.com>
2021-02-11 16:37:03 -0500

    Fix midd findNote

--- src/apps/backend/middlewares/note/findNote.midd.ts
+++ src/apps/backend/middlewares/note/findNote.midd.ts
@@ -2,18 +2,9 @@ import { header, param, query } from 'express-validator';
 import { validRouterExpressValidator } from './../shared/expressValidatorValidRoute';
 
 export function findNoteAllMidd() {
-  return [
-    header('authorization').exists().isUUID(),
-    query('limit').optional().isInt(),
-    query('page').optional().isInt(),
-    validRouterExpressValidator,
-  ];
+  return [query('limit').optional().isInt(), query('page').optional().isInt(), validRouterExpressValidator];
 }
 
 export function findNoteByIdNotebook() {
-  return [
-    header('authorization').exists().isUUID(),
-    param('idNotebook').exists().isUUID(),
-    validRouterExpressValidator,
-  ];
+  return [param('idNotebook').exists().isUUID(), validRouterExpressValidator];
 }
