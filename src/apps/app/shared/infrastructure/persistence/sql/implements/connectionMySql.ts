1458441fde8d811fe95cec345c52cd4c3e3ee47f
Mozzarth <moisescaicedo15@gmail.com>
2021-02-11 15:51:24 -0500

    Quite console.log de los on de connection mysql

--- src/apps/app/shared/infrastructure/persistence/sql/implements/connectionMySql.ts
+++ src/apps/app/shared/infrastructure/persistence/sql/implements/connectionMySql.ts
@@ -1,61 +1,58 @@
-import { SqlConnection } from "../IConnectionSql";
-import mysql, { PoolConfig } from 'mysql'
-import { CONFIG } from "../../../../../config";
-
-
+import { SqlConnection } from '../IConnectionSql';
+import mysql, { PoolConfig } from 'mysql';
+import { CONFIG } from '../../../../../config';
 
 class ConnectionMySql implements SqlConnection {
+  private connectionPool: mysql.Pool;
 
-    private connectionPool: mysql.Pool
-
-    constructor() {
-        const configConn: PoolConfig = {
-            connectionLimit: 15, // Determina el limite de conexiones
-            queueLimit: 50, // Determina el limite de conexiones en cola antes de mandar un error
-            waitForConnections: true, // Permite conexiones en cola 
-            timezone: "-5000", // zona horaria
-            insecureAuth: true, // Permitir la conexión a instancias de MySQL que soliciten el método de autenticación antiguo
-            host: CONFIG.DB_HOST,
-            user: CONFIG.DB_USER,
-            password: CONFIG.DB_PASS,
-            port: Number(CONFIG.DB_PORT),
-            database: CONFIG.DB_DATABASE,
+  constructor() {
+    const configConn: PoolConfig = {
+      connectionLimit: 15, // Determina el limite de conexiones
+      queueLimit: 50, // Determina el limite de conexiones en cola antes de mandar un error
+      waitForConnections: true, // Permite conexiones en cola
+      timezone: '-5000', // zona horaria
+      insecureAuth: true, // Permitir la conexión a instancias de MySQL que soliciten el método de autenticación antiguo
+      host: CONFIG.DB_HOST,
+      user: CONFIG.DB_USER,
+      password: CONFIG.DB_PASS,
+      port: Number(CONFIG.DB_PORT),
+      database: CONFIG.DB_DATABASE,
+    };
+    this.connectionPool = mysql.createPool(configConn);
+    this.registerEvents();
+  }
+  public getConnection(): Promise<mysql.Connection> {
+    return new Promise((res, rej) => {
+      this.connectionPool.getConnection((err, connection) => {
+        if (err) {
+          rej(err);
         }
-        this.connectionPool = mysql.createPool(configConn)
-        this.registerEvents()
-    }
-   public getConnection(): Promise<mysql.Connection> {
-        return new Promise((res, rej) => {
-            this.connectionPool.getConnection((err, connection) => {
-                if (err) { rej(err) }
-                res(connection)
-            })
-        })
-    }
-   public    close(): void {
-        // Deberia der una promesa 
-        // cierra todas las coexiones del pool
-        this.connectionPool.end()
-    }
+        res(connection);
+      });
+    });
+  }
+  public close(): void {
+    // Deberia der una promesa
+    // cierra todas las coexiones del pool
+    this.connectionPool.end();
+  }
 
+  private registerEvents() {
+    this.connectionPool.on('acquire', function (connection) {
+      //console.log('Connection %d acquired || Conexion Adquirida', connection.threadId);
+    });
+    this.connectionPool.on('enqueue', function () {
+      //console.log('Waiting for available connection slot || Esperando por una conexión');
+    });
+    this.connectionPool.on('connection', function (connection) {
+      // connection.query('SET SESSION auto_increment_increment=1')
+      //console.log("New connection established  || Nueva conexión establecida")
+    });
 
-    private registerEvents() {
-        this.connectionPool.on('acquire', function (connection) {
-            console.log('Connection %d acquired || Conexion Adquirida', connection.threadId);
-        });
-        this.connectionPool.on('enqueue', function () {
-            console.log('Waiting for available connection slot || Esperando por una conexión');
-        });
-        this.connectionPool.on('connection', function (connection) {
-            // connection.query('SET SESSION auto_increment_increment=1')
-            console.log("New connection established  || Nueva conexión establecida")
-        });
-
-        this.connectionPool.on('release', function (connection) {
-            console.log('Connection %d released || conexión liberada', connection.threadId);
-
-        });
-    }
+    this.connectionPool.on('release', function (connection) {
+      //console.log('Connection %d released || conexión liberada', connection.threadId);
+    });
+  }
 }
 
-export = new ConnectionMySql()
+export = new ConnectionMySql();
