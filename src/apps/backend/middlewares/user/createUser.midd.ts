004b4fb71e53364d627c7a14f30c2bf175d5d0a8
Mozzarth <moisescaicedo15@gmail.com>
2021-02-16 10:46:17 -0500

    add validation password min 8 character

--- src/apps/backend/middlewares/user/createUser.midd.ts
+++ src/apps/backend/middlewares/user/createUser.midd.ts
@@ -4,7 +4,7 @@ import { validRouterExpressValidator } from '../shared';
 function createUserMidd() {
   return [
     body('email').isString().exists().isEmail(),
-    body('password').isString().exists(),
+    body('password').isString().exists().isLength({ min: 8 }).withMessage('minimo 8 caracteres'),
     validRouterExpressValidator,
   ];
 }
