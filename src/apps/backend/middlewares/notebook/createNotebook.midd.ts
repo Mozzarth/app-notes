5dbbbede2b95f71ebaa56f36e093078ef43cabb0
Mozzarth <moisescaicedo15@gmail.com>
2021-02-16 08:23:31 -0500

    create notebook title min 1 validation

--- src/apps/backend/middlewares/notebook/createNotebook.midd.ts
+++ src/apps/backend/middlewares/notebook/createNotebook.midd.ts
@@ -2,5 +2,5 @@ import { validRouterExpressValidator } from '../shared';
 import { body } from 'express-validator';
 
 export function createNotebookMid() {
-  return [body('title').isString().exists(), validRouterExpressValidator];
+  return [body('title').isString().isLength({ min: 1 }).exists(), validRouterExpressValidator];
 }
