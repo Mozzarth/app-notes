b0dd7812b0ea3ea99a0eb8a4eb187d4f04d064b9
Mozzarth <moisescaicedo15@gmail.com>
2021-02-16 16:09:36 -0500

    add scripts start y fix build script

--- src/apps/app/user/activeUser/application/index.ts
+++ src/apps/app/user/activeUser/application/index.ts
@@ -1,9 +1,7 @@
-import { ActiveUserMySqlRepository } from '../infrastructure/activeUserMySqlRepository'
-import { ActiveUserUseCase } from './activeUserUseCase'
+import { ActiveUserMySqlRepository } from '../infrastructure/activeUserMySqlRepository';
+import { ActiveUserUseCase } from './activeUserUseCase';
 
-
-
-const activeUserRepository = new  ActiveUserMySqlRepository()
+const activeUserRepository = new ActiveUserMySqlRepository();
 const activeUser = new ActiveUserUseCase(activeUserRepository);
 
-export {activeUser}
+export { activeUser };
