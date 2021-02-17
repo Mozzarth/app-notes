839422a653ec39f338cedb5e44cbc3613dee3ba2
Mozzarth <moisescaicedo15@gmail.com>
2021-02-17 07:30:26 -0500

    console.log del config

--- src/apps/backend/server.ts
+++ src/apps/backend/server.ts
@@ -1,6 +1,7 @@
 import { CONFIG } from './config/config';
 import app from './app';
 
+console.log({ CONFIG });
 app.listen(CONFIG.SERVER_PORT, () => {
   console.log(`App is running at http://localhost:${CONFIG.SERVER_PORT} in ${process.env.NODE_ENV} mode`);
 });
