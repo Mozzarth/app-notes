71a15481af49aae3f46888825f7f87c3a3b068f1
Moises David Caicedo Corena <moisescaicedo15@gmail.com>
2021-02-17 07:20:50 -0500

    quitando console.log

--- src/apps/backend/server.ts
+++ src/apps/backend/server.ts
@@ -1,7 +1,6 @@
 import { CONFIG } from './config/config';
 import app from './app';
 
-console.log(process.env);
 app.listen(CONFIG.SERVER_PORT, () => {
   console.log(`App is running at http://localhost:${CONFIG.SERVER_PORT} in ${process.env.NODE_ENV} mode`);
 });
