fa67bb47b63db3600b11da86c87e1e2fbfd966f9
Mozzarth <moisescaicedo15@gmail.com>
2021-02-16 20:19:42 -0500

    change name variable node_port to port in process.env

--- src/apps/backend/config/config.ts
+++ src/apps/backend/config/config.ts
@@ -1,14 +1,13 @@
-import { config } from 'dotenv'
+import { config } from 'dotenv';
 
-if (process.env.NODE_ENV != "production") {
-    const con = config()
-    if (con.error) {
-        console.error("Ocurrio un error a la hora de configurar entorno")
-        process.exit(1)
-    }
+if (process.env.NODE_ENV != 'production') {
+  const con = config();
+  if (con.error) {
+    console.error('Ocurrio un error a la hora de configurar entorno');
+    process.exit(1);
+  }
 }
-
 
 export const CONFIG = {
-    SERVER_PORT: process.env.NODE_PORT
-}
+  SERVER_PORT: process.env.PORT,
+};
