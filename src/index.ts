9de4e73184eb9ef405c03c4b61ce21bbba3f34c7
Mozzarth <moisescaicedo15@gmail.com>
2021-02-19 13:56:45 -0500

    ---

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { CONFIG } from './apps/backend/config/config';
 import app from './apps/backend/app';
 
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
+app.set('port', process.env.PORT);
 app.listen(CONFIG.SERVER_PORT, () =>
-  console.log(`Server is running on port: ${CONFIG.SERVER_PORT} in ${CONFIG.ENV_APP} mode`)
+  console.log(`Server is running on port: ${app.get('port')} in ${CONFIG.ENV_APP} mode`)
 );
