3b0b14bb75943a0c932890b800c44e6c277d11c0
Mozzarth <moisescaicedo15@gmail.com>
2021-02-19 10:23:28 -0500

    ADD cors in backend

--- src/apps/backend/app.ts
+++ src/apps/backend/app.ts
@@ -3,9 +3,11 @@ import { appRouter } from './routes';
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
