4fe96690fa18e147345f8d423aec020bb1ad8f11
Mozzarth <moisescaicedo15@gmail.com>
2021-02-19 11:20:33 -0500

    ADD env.port al init del server

--- src/apps/backend/server.ts
+++ src/apps/backend/server.ts
@@ -1,6 +1,6 @@
 import { CONFIG } from './config/config';
 import app from './app';
 
-app.listen(CONFIG.SERVER_PORT, () => {
-  console.log(`App is running at http://localhost:${CONFIG.SERVER_PORT} in ${CONFIG.ENV_APP} mode`);
+app.listen(process.env.PORT, () => {
+  console.log(`App is running at http://localhost:${process.env.PORT} in ${CONFIG.ENV_APP} mode`);
 });
